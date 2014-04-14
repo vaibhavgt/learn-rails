8bb37b77720cfc16863c66daa202fe1190628cfe
vaibhavgt <vaibhavspacegupta@gmail.com>
2014-04-14 22:18:38 +0530

    revised app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,23 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # email enabled in production
+    config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"] }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.default :charset => "utf-8"
+    config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["DOMAIN_NAME"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+    }
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
