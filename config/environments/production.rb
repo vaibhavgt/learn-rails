6a508ba9c4b2e543de37cab7af034c5ca29428bc
vaibhavgt <vaibhavspacegupta@gmail.com>
2014-04-14 22:55:48 +0530

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    password: ENV["rockonisgreat"]
     }
 
 
